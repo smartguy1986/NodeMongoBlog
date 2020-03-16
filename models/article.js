2dc4059ab6cf88360c9a26f11db6f4c2a4cecafc
smartguy1986 <arijit.nandi.2008@gmail.com>
2020-03-16 18:57:20 +0530

    Blog add and home page list view

---
+++ models/article.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb://root:1986@blog-shard-00-00-eip6j.mongodb.net:27017,blog-shard-00-01-eip6j.mongodb.net:27017,blog-shard-00-02-eip6j.mongodb.net:27017/test?ssl=true&replicaSet=blog-shard-0&authSource=admin&retryWrites=true&w=majority')
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Article', articleSchema)
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+mongoose.connect('mongodb://root:1986@blog-shard-00-00-eip6j.mongodb.net:27017,blog-shard-00-01-eip6j.mongodb.net:27017,blog-shard-00-02-eip6j.mongodb.net:27017/test?ssl=true&replicaSet=blog-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false}))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt: 'desc'})
+    res.render('articles/index', { articles: articles})
+})
+
+app.use('/articles/', articleRouter)
+
+app.listen(5000)
